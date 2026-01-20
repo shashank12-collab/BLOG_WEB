98ac13700bdd7ac83100d519dc9621c9b9daab7f
shashank <shashanksingh56074@gmail.com>
2026-01-20 22:04:23 +0530

    connecting

---
+++ blogapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request , 'index.html')
+
+def aboutus(request):
+    return render(request , 'aboutus.html')
