2f14fc196912b9ffbf7ea0f2dd3be0c5b5efa3cb
shashank <shashanksingh56074@gmail.com>
2026-01-22 00:47:24 +0530

    Blog app models and migrations

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,23 @@
+from django.utils import timezone
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Post(models.Model):   # Capital P (best practice)
+
+    options = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=150, blank=True)
+    publish = models.DateField(default=timezone.now)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_post',
+        null=True,
+        blank=True
+    )
+    content = models.TextField(blank=True)
+    status = models.CharField(max_length=10, choices=options, default='draft')
